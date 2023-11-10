48ce7f246e25ec39c643401d44077ecced631eb8
Manohar <manohar12guddala@gmail.com>
2023-11-10 16:54:35 +0530

    Endpoint configuration

---
+++ lib/data/config.dart
@@ -0,0 +1,6 @@
+class Config {
+  static const String apiKey = "bff9be2f-bce0-4680-8bbe-846b130d0c7b";
+  static const String baseUrl = "https://pro-api.coinmarketcap.com/";
+  static const String latest = "v1/cryptocurrency/listings/latest";
+  static const String info = "v2/cryptocurrency/info";
+}
