e95bc650da3c7e28655ef06535f5a8a1bc0ef838
Manohar195 <manohar@awebdigital.co>
2023-11-20 18:42:32 +0530

    Added one more events for crypto loading

--- lib/presentation/bloc/CryptoEvent.dart
+++ lib/presentation/bloc/CryptoEvent.dart
@@ -1,5 +1,7 @@
 import 'package:equatable/equatable.dart';
 
+import '../../domain/entities/CryptoListing.dart';
+
 abstract class CryptoEvent extends Equatable {
   const CryptoEvent();
   @override
@@ -10,4 +12,11 @@ class FetchNextCryptosList extends CryptoEvent {
   const FetchNextCryptosList();
   @override
   List<Object?> get props => [];
+}
+
+class CryptoListFetched extends CryptoEvent {
+  const CryptoListFetched(this.listing);
+  final List<CryptoListing> listing;
+  @override
+  List<Object?> get props => [listing];
 }
