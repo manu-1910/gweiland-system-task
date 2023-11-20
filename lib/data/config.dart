2341b79e8d84a044b3515ba6bb0936436d28a17c
Manohar195 <manohar@awebdigital.co>
2023-11-20 18:39:49 +0530

    updated endpoints

--- lib/data/config.dart
+++ lib/data/config.dart
@@ -1,6 +1,6 @@
 class Config {
   static const String apiKey = "bff9be2f-bce0-4680-8bbe-846b130d0c7b";
   static const String baseUrl = "https://pro-api.coinmarketcap.com/";
-  static const String latest = "v1/cryptocurrency/listings/latest";
-  static const String info = "v2/cryptocurrency/info";
+  static const String latest = "v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=${apiKey}&limit=20";
+  static const String info = "v2/cryptocurrency/info?CMC_PRO_API_KEY=${apiKey}";
 }
